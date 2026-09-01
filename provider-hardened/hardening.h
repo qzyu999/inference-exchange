644beb7806b3517d204529c5902dff8a24b84e96
Jared Yu <qzyu999@gmail.com>
2026-08-31 17:48:55 -0700

    Add extern C to hardening.h for C++ linkage

--- provider-hardened/hardening.h
+++ provider-hardened/hardening.h
@@ -1,6 +1,10 @@
 #ifndef OCIP_HARDENING_H
 #define OCIP_HARDENING_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /**
  * Apply OCIP security hardening to the current process.
  * Must be called as the FIRST thing in main(), before loading models.
@@ -11,5 +15,9 @@
  * On other platforms: no-op (returns 0).
  */
 int ocip_harden(void);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* OCIP_HARDENING_H */
